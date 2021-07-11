b1aad1ea710fb1887fab90b17aa70db1f12424ff
Yathaarth Batra <yathaarthbatra10@gmail.com>
2021-07-11 19:09:31 +0530

    Completed Commit

--- app/src/main/java/com/example/newstime/NewsDetailActivity.java
+++ app/src/main/java/com/example/newstime/NewsDetailActivity.java
@@ -2,11 +2,22 @@ package com.example.newstime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class NewsDetailActivity extends AppCompatActivity {
 
     private String title,des,content,imageUrl,url;
+    private TextView titleView,contentView,desView;
+    private ImageView imageView;
+    private Button buttonView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,29 @@ public class NewsDetailActivity extends AppCompatActivity {
         content=getIntent().getStringExtra("content");
         imageUrl=getIntent().getStringExtra("imageUrl");
         url=getIntent().getStringExtra("url");
+
+        //referring the views
+        titleView=findViewById(R.id.newsDetailTitle);
+        contentView=findViewById(R.id.newsDetailContent);
+        desView=findViewById(R.id.newsDetailDesc);
+        imageView=findViewById(R.id.newsDetailImage);
+        buttonView=findViewById(R.id.newsDetailButton);
+
+        //inserting the data into the views
+        titleView.setText(title);
+        contentView.setText(content);
+        desView.setText(des);
+        Picasso.get().load(imageUrl).into(imageView);
+        buttonView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //will define implicit intent
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+
+            }
+        });
 
 
     }
